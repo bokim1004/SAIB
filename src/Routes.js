21dd851e0ef635d267181b4fffdc824a838b1115
김보경 <akbk1004@gmail.com>
2021-01-31 17:38:41 +0900

    Add:nav완성

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
-import Main from './Pages/Main/Main';
+import Main from "./Pages/Main/Main";
+import Nav from "./Pages/Main/Components/Nav/Nav";
 
 class Routes extends Component {
-  render () {
+  render() {
     return (
       <Router>
         <Route exact path="/" component={Main} />
+        <Route exact path="/Nav" component={Nav} />
       </Router>
     );
   }
